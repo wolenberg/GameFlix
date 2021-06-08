8120918b00732ac54937a94ea24896d9a1f583e9
wolenberg <wolembergjp@gmail.com>
2021-06-08 12:22:32 -0300

    initial commit

---
+++ js/owl/setup.js
@@ -0,0 +1,86 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+function TextoImg01() {
+    document.getElementById("descricao").innerHTML = "Depois de tudo dar errado durante um roubo em uma cidade do oeste chamada Blackwater, Arthur Morgan e a gangue Van der Linde são forçados a fugir. Com agentes federais e os melhores caçadores de recompensas no seu encalço, a gangue precisa roubar, assaltar e lutar para sobreviver no impiedoso coração dos Estados Unidos.";
+    document.getElementById("titulo").innerHTML = "RED DEAD REDEMPTION 2"
+    document.documentElement.style.setProperty('--url',"url('/img/capa-rdr2.png')")
+}
+
+function TextoImg02() {
+    document.getElementById("descricao").innerHTML = "The Last of Us é uma experiência de gênero definitiva que mistura elementos de sobrevivência e ação para contar uma história de personagens sobre uma praga moderna dizimando a humanidade. A natureza invade a civilização, forçando os sobreviventes restantes a matarem por comida, armas e qualquer coisa que possam achar.";
+    document.getElementById("titulo").innerHTML = "THE LAST OF US"
+    document.documentElement.style.setProperty('--url',"url('/img/capa-tlou.jpg')")
+    
+}
+
+function TextoImg03() {
+    document.getElementById("descricao").innerHTML = "As aventuras de Kratos e seu filho Atreus na jornada mais emocionante e instigante de God Of War Kratos, o Deus da Guerra, deixou para trás sua vingança contra os deuses do Olimpo. ... Mais do que lutar pela sua própria sobrevivência, deve ensinar seu filho Atreus a conviver em um mundo hostil e cheio de desafios.";
+    document.getElementById("titulo").innerHTML = "GOD OF WAR 2018"
+    document.documentElement.style.setProperty('--url',"url('/img/capa-gow.jpg')")
+    
+}
+
+function TextoImg04() {
+    document.getElementById("descricao").innerHTML = "Gears 5 foca em Kait Diaz (Laura Bailey), uma descendente da estranha Locust. Como Kait, o jogador deve descobrir as origens da família de Locust e Kait. O protagonista de Gears of War 4, JD Fenix (Liam McIntyre), seu amigo Delmont Walker (Eugene Byrd) e o pai de JD, Marcus Fenix (John DiMaggio) também retornam.";
+    document.getElementById("titulo").innerHTML = "GEARS 5"
+    document.documentElement.style.setProperty('--url',"url('/img/capa-gears.jpg')")
+    
+}
+
+function TextoImg05() {
+    document.getElementById("descricao").innerHTML = "A proposta de Forza Horizon 4 é te levar para uma competição em uma região que é apaixonada por velocidade. O Reino Unido, mesmo que representado de uma forma fictícia em um mapa reduzido, ainda sim consegue passar a sensação de estarmos disputando provas nas mais belas estradas do mundo.";
+    document.getElementById("titulo").innerHTML = "FORZA HORIZON 4"
+    document.documentElement.style.setProperty('--url',"url('/img/capa-forza.jpg')")
+    
+}
+
+function TextoImg06() {
+    document.getElementById("descricao").innerHTML = "Dragon Quest XI - Echoes of an Elusive Age segue a jornada de um herói caçado pelo mundo de Erdrea. Ele deve descobrir o mistério sobre seu destino e cumprir a profecia sobre seu nascimento. Para isso, o protagonista conta com a ajuda de um elenco de coadjuvantes muito carismáticos.";
+    document.getElementById("titulo").innerHTML = "DRAGON QUEST XI"
+    document.documentElement.style.setProperty('--url',"url('/img/capa-dgxi.jpg')")
+    
+}
+
+function TextoImg07() {
+    document.getElementById("descricao").innerHTML = "Nocauteado em uma briga com Chris, Ethan acorda em um vilarejo completamente em ruínas, que parece ter especial apreço por uma figura quase religiosa chamada Mother Miranda. Lá, ele inicia sua jornada em busca de Rosemary, enfrentando vários inimigos e desvendando inúmeros segredos.";
+    document.getElementById("titulo").innerHTML = "RESIDENT EVIL VILLAGE"
+    document.documentElement.style.setProperty('--url',"url('/img/capa-re8.jpg')")
+    
+}
+
+function TextoImg08() {
+    document.getElementById("descricao").innerHTML = "Apresenta dois corredores em um só kart, itens especiais, jogabilidade cooperativa e multiplayer via LAN. ... Mario Kart 8 apresenta seções antigravidade, quadriciclos e Mario Kart TV. É também o primeiro jogo da série a ter conteúdo adicional por download (DLC) e introduzir o novo modo de velocidade 200cc.";
+    document.getElementById("titulo").innerHTML = "MARIO KART 8"
+    document.documentElement.style.setProperty('--url',"url('/img/capa-mk8.jpg')")
+    
+}
+
+function TextoImg09() {
+    document.getElementById("descricao").innerHTML = "The Legend of Zelda: Breath of the Wild é um jogo eletrônico de ação-aventura em mundo aberto que os jogadores podem explorar livremente enquanto controlam Link. ... O jogador pode realizar ações como correr, escalar, nadar e voar com um paraglider, porém todas são limitadas pelo fôlego de Link.";
+    document.getElementById("titulo").innerHTML = "THE LEGEND OF ZELDA"
+    document.documentElement.style.setProperty('--url',"url('/img/capa-zelda.jpg')")
+    
+}
+
+function TextoImg10() {
+    document.getElementById("descricao").innerHTML = "Uma guerra pelo poder. Uma guerra pelo controle da Nova York da Marvel estourou entre uma empresa de energia desonesta e um exército criminoso de alta tecnologia. Com o seu novo lar ameaçado pelo conflito, Miles deve descobrir o preço de se tornar um herói e decidir o que sacrificar pelo bem maior.";
+    document.getElementById("titulo").innerHTML = "SPIDERMAN - Miles Morales"
+    document.documentElement.style.setProperty('--url',"url('/img/capa-spider.jpg')")
+    
+}
